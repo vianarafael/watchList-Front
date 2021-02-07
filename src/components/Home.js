cc40f9ad0bc3979cf80203158b72cf68fb675f5f
vianarafael <vianarafaelds@gmail.com>
2021-02-07 09:48:33 +0900

    adding slider

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,10 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import env from 'react-dotenv'
+import env from 'react-dotenv';
+
+import MySlider from './MySlider'
+
+
 
 const Home = () => {
     const [upcoming, setUpcoming] = useState([]);
@@ -8,12 +12,11 @@ const Home = () => {
     useEffect(() => {
         const getMovies = async() => {
             const response = await axios.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=${env.KEY}&language=en-US&page=1`);
-            console.log(response)
             setUpcoming(response.data.results);
         }
         getMovies()
     }, [])
-    return <ul>{upcoming.length ? upcoming.map(film => <li key={film.id}>{film.title}</li>) : ''}</ul>
+    return <MySlider movies={upcoming} />
 };
 
 export default Home;
