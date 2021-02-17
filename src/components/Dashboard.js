aa962d75db333d0772f058441f66de06fee2f51d
vianarafael <vianarafaelds@gmail.com>
2021-02-17 16:56:19 +0900

    started displaying the movies in the dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import env from 'react-dotenv'
 
 const Dashboard = ({name}) => {
   const [watchList, setWatchList] = useState([]);
     // I have the user id on localStorage
 
     // I will access the movies from the database using the user's id
-
+    // testing
     useEffect(() => {
       async function getUserData() {
         const authenticationResult = await axios.get("/isAuth", {
@@ -19,12 +20,24 @@ const Dashboard = ({name}) => {
           const uid = localStorage.getItem("uid")
           const getMovies = await axios.get(`/movies/${uid}`)
           // now I have the id of the movies - I need to turn this into something more useful
-          setWatchList(getMovies.data.rows);
+        
+          const moviesData = await Promise.all(getMovies.data.rows.map((film) => axios.get(`https://api.themoviedb.org/3/movie/${film.mid}?api_key=${env.KEY}&append_to_response=videos,credits`)
+    
+          )
+        )
+        setWatchList(moviesData);
+          }
         }
-      }
-
       getUserData();
     })
+
+    const displayMovies = () => {
+      return watchList.map(film => (
+        <>
+          <p>{film.data.title}</p>
+        </>
+      ))
+    }
     return ( 
     <>
       <h1>Dashboard</h1>
@@ -34,6 +47,9 @@ const Dashboard = ({name}) => {
         localStorage.removeItem("uid");
       }
     }>Clear Local Storage</button>
+      <>
+       {watchList ? displayMovies() : 'loading'}
+      </>
     </>
     )
 }
